6dd30bf9822cf1bac10c66bd891341949569f5c8
Danilo Albuquerque <danilo.albuquerque@liferay.com>
2021-10-22 17:05:16 -0300

    adding for the first time the .sh file

---
+++ subtakeover.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# You can use this tool to search for subdomains.
+# I created it to use as my own tool to find Subdomain Takeover
+# vulnerabilities.
+# You might are asking your self "how can I do use it to do that?".
+# Let me explain. It will search for subdomains and will filter the
+# alias for rows. When we find something dealing with clouds, we can
+# test the Subdomain Takeover attack.
+
+echo "---------------------------------------------------------------"
+echo  "____    ___        ___   _"
+echo "|  _ \  / \ \      / / \ | |"
+echo "| | | |/ _ \ \ /\ / /|  \| |"
+echo "| |_| / ___ \ V  V / | |\  |"
+echo "|____/_/   \_\_/\_/  |_| \_|"
+echo ""
+
+echo "hello, have a great hacking! if you need something, you can"
+echo "send me a direct message in my GitHub account."
+echo "https://github.com/daniloalbuqrque"
+echo "_______________________________________________________________"
+echo ""
+echo "Get there and see my others repos, you might find something"
+echo "interesting."
+echo ""
+echo ""
+
+echo "for the [ -a ] option we'll have to specify an URL, for example:"
+echo ""
+echo "./bruteforcedomains.sh -a URL [e.g. google.com]"
+echo ""
+
+while [ -n "$1" ]
+ do
+  case "$1" in
+     -a)
+	for word in $(cat 11m_sub_wordlist.txt);do
+	host -t cname $word.$1 | grep -v "alias for"
+	done;;
+     -b) echo "I'm still doing it.";;
+     -c) echo "Keep calm.";;
+     -d) echo "Relax and you the first option for awhile";;
+     -e) echo "To be continued";;
+     -f) echo "Wait for more";;
+  esac
+ shift
+done
